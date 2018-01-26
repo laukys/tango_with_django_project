44bea1166a1cfe18f1ee8e05622f31b85a18409b
Dominykas Jakstonis <2146735j@student.gla.ac.uk>
2018-01-26 05:03:53 +0000

    2nd commit

---
+++ rango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "Fat Cat page"}
+    return render(request, 'rango/about.html', context=context_dict)
