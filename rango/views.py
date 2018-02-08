5518779ad86aa826e71809831301b85efe2ce515
Dominykas Jakstonis <2146735j@student.gla.ac.uk>
2018-02-08 13:11:44 +0000

    lets go1

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
-
+from rango.models import Page
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -17,6 +17,24 @@ def index(request):
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
+
 def about(request):
     context_dict = {'boldmessage': "Fat Cat page"}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
